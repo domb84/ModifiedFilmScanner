40db3632a9a59b0bcc32caa85ea4b2fc9f65faf3
Dominic Bird <dominic@dominicbird.com>
2025-06-08 20:57:38 +0100

    - A working firmware modify script

--- autoModify.ps1
+++ autoModify.ps1
@@ -1,26 +1,30 @@
 # Define paths
 
 # application paths
+$java = "C:\Program Files\OpenJDK\jdk-22.0.2\bin\java.exe"
 $bfc4ntk = "C:\Users\DominicBird\Mega\ch341\M127 ROM\bfc4ntk.exe"
 $ntkcalc = "C:\Users\DominicBird\Mega\ch341\M127 ROM\ntkcalc.exe"
 $ntkmpe = "C:\Users\DominicBird\Mega\ch341\Tools\NtkMPE.jar"
-$java = "C:\Program Files\OpenJDK\jdk-22.0.2\bin\java.exe"
 
 # source paths
 $source = "C:\Users\DominicBird\Mega\ch341\M127 ROM\verified_dump_2025.6.6.2.bin"
+
+# extract filename and path
 $sourceFilename = [io.path]::GetFileNameWithoutExtension($source)
 $sourcePath = [io.path]::GetDirectoryName($source)
-$part1Offset = "000af000"
-$bootloaderOffset = "000008f8"
-$endOffset = "0024AE38"
 
-# variable paths
+# partition offsets
+$suffixOffset = "0x4000"
+$part1Offset = "000ab000"
+
+# temp and output paths
 $extractedFile = "$sourcePath\$sourceFilename.decomp"
 $compressedFile = "$sourcePath\$sourceFilename.compressed"
-$outputFile = "$sourcePath\$sourceFilename.modified.bin"
+$suffixFile = "$sourcePath\$sourceFilename.suffix"
+$outputFile = "$sourcePath\FWDV180N.BIN"
 
 # Put them in an array
-$paths = @($bfc4ntk, $ntkcalc, $ntkmpe, $source)
+$paths = @($bfc4ntk, $ntkcalc, $ntkmpe, $source. $java)
 
 # Check existence
 foreach ($path in $paths) {
@@ -30,8 +34,15 @@ foreach ($path in $paths) {
     }
 }
 
-# extract the partition
-$arguments = "-x `"$source`" `"$extractedFile`" `"$part1Offset`""
+# Read the bytes after the offset
+$suffixOffset = [Convert]::ToInt32($suffixOffset, 16)
+$allBytes = [System.IO.File]::ReadAllBytes($source)
+$suffix = $allBytes[$suffixOffset..($allBytes.Length - 1)]
+# Write to the output file
+[System.IO.File]::WriteAllBytes($suffixFile, $suffix)
+
+# extract the partition from the valid suffix file
+$arguments = "-x `"$suffixFile`" `"$extractedFile`" `"$part1Offset`""
 Write-Host "Running: $bfc4ntk $arguments"
 Start-Process -NoNewWindow -FilePath $bfc4ntk -ArgumentList "$arguments" -Wait
 
@@ -41,14 +52,15 @@ if (-Not (Test-Path $extractedFile)) {
     exit 1
 }
 
-# open ntkmpe to modify the partition
-Start-Process -FilePath $java -ArgumentList "-jar", $ntkmpe, $extractedFile -Wait
+# # open ntkmpe to modify the partition
+Start-Process -FilePath $java -ArgumentList "-jar", $ntkmpe -Wait
 
 # wait for it to be modified
-# Read-Host "Press Enter after you have modifed the partition $extractedFile"
+Read-Host "Press Enter after you have modifed the partition $extractedFile"
 
 # compress the modified partition
 $arguments = "-p `"$extractedFile`" `"$compressedFile`" `"$part1Offset`""
+# $arguments = "-c `"$extractedFile`" `"$compressedFile`""
 Write-Host "Running: $bfc4ntk $arguments"
 Start-Process -NoNewWindow -FilePath $bfc4ntk -ArgumentList "$arguments" -Wait
 
@@ -58,51 +70,18 @@ if (-Not (Test-Path $compressedFile)) {
     exit 1
 }
 
-# # Read everything before offset
-# $offset = [Convert]::ToInt32($bootloaderOffset, 16)
-# # Read the bytes before the offset
-# $prefix = [System.IO.File]::ReadAllBytes($source)[0..($offset - 1)]
-# # Read the content of the nmodified partition
-# $main = [System.IO.File]::ReadAllBytes($compressedFile)
-# # Combine the two
-# $combined = $prefix + $main
-# # Write to the output file
-# [System.IO.File]::WriteAllBytes($outputFile, $combined)
-# # Check that output file exists
-# if (-Not (Test-Path $outputFile)) {
-#     Write-Host "Output file not found: $outputFile" -ForegroundColor Red
-#     exit 1
-# } else {
-#     Write-Host "Output file created successfully: $outputFile" -ForegroundColor Green
-# }
+# checksum entire file and write to output file
+$arguments = "-cw `"$compressedFile`""
+Write-Host "Running: $ntkcalc $arguments"
+Start-Process -NoNewWindow -FilePath $ntkcalc -ArgumentList "$arguments" -Wait
 
-# Read after before offset
-$offset = [Convert]::ToInt32($endOffset, 16)
-# Read the bytes after the offset
-$suffix = $allBytes[$offset..($allBytes.Length - 1)]
-# read the content of the compressed partition
-$main = [System.IO.File]::ReadAllBytes($compressedFile)
-# Combine the two
-$combined = $main + $suffix
-# Write to the output file
-[System.IO.File]::WriteAllBytes($outputFile, $combined)
-# Check that output file exists
-if (-Not (Test-Path $outputFile)) {
-    Write-Host "Output file not found: $outputFile" -ForegroundColor Red
-    exit 1
+# Check if the checksum calculation was successful
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Checksum recalculated successfully." -ForegroundColor Green
 } else {
-    Write-Host "Output file created successfully: $outputFile" -ForegroundColor Green
+    Write-Host "Checksum recalculation failed." -ForegroundColor Red
+    exit 1
 }
 
-
-# $arguments = "-cw `"$outputFile`""
-# Write-Host "Running: $ntkcalc $arguments"
-# Start-Process -NoNewWindow -FilePath $ntkcalc -ArgumentList "$arguments" -Wait
-
-# # Check if the checksum calculation was successful
-# if ($LASTEXITCODE -ne 0) {
-#     Write-Host "Checksum recalculated successfully." -ForegroundColor Green
-# } else {
-#     Write-Host "Checksum recalculation failed." -ForegroundColor Red
-#     exit 1
-# }
+# Create the final file
+Copy-Item -Path $compressedFile -Destination $outputFile -Force
---
+++ scratch.ps1
@@ -0,0 +1,41 @@
+
+# $bootloaderOffset = "0x8f8"
+# # Combine firmware and suffix
+# # read the content of the compressed partition
+# $main = [System.IO.File]::ReadAllBytes($compressedFile)
+# # Combine the two
+# $combined = $main + $suffix
+# [System.IO.File]::WriteAllBytes($outputFile, $combined)
+# # Check that output file exists
+# if (-Not (Test-Path $outputFile)) {
+#     Write-Host "Output file not found: $outputFile" -ForegroundColor Red
+#     exit 1
+# } else {
+#     Write-Host "Output file created successfully: $outputFile" -ForegroundColor Green
+# }
+
+
+# # open ntkmpe to modify the partition
+# Start-Process -FilePath $java -ArgumentList "-jar", $ntkmpe, $extractedFile -Wait
+
+# # wait for it to be modified
+# # Read-Host "Press Enter after you have modifed the partition $extractedFile"
+
+
+# # Read everything before offset
+# $offset = [Convert]::ToInt32($bootloaderOffset, 16)
+# # Read the bytes before the offset
+# $prefix = [System.IO.File]::ReadAllBytes($source)[0..($offset - 1)]
+# # Read the content of the nmodified partition
+# $main = [System.IO.File]::ReadAllBytes($compressedFile)
+# # Combine the two
+# $combined = $prefix + $main
+# # Write to the output file
+# [System.IO.File]::WriteAllBytes($outputFile, $combined)
+# # Check that output file exists
+# if (-Not (Test-Path $outputFile)) {
+#     Write-Host "Output file not found: $outputFile" -ForegroundColor Red
+#     exit 1
+# } else {
+#     Write-Host "Output file created successfully: $outputFile" -ForegroundColor Green
+# }
